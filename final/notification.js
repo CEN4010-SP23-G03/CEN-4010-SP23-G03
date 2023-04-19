83f87815b04fd60219ff4030239de8c29b172d01
Yu Harukaze <49761904+setsuharu@users.noreply.github.com>
2023-04-19 14:33:15 -0400

    begin m5 implementation, new backend features listed in changes.txt

---
+++ final/notification.js
@@ -0,0 +1,14 @@
+function set_notification_success()
+{
+    var notification_box = $("#thumbnail-grid-notification-area");
+    notification_box.html("");
+    notification_box.append(
+        `
+            <div class="alert alert-success d-flex align-items-center alert-dismissible fade show" role="alert">
+                <svg class="bi flex-shrink-0 me-2" width="24" height="24" role="img" aria-label="Success:"><use xlink:href="#check-circle-fill"/></svg>
+                <div>Image saved successully!</div>.
+                <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+            </div>
+        `
+    )
+}
